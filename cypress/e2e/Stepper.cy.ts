bfad8bbf841397d8472b05667dd2babc56b5f20c
Nikola <37514211+nikola011381@users.noreply.github.com>
2022-07-30 17:10:34 +0200

    stepper commit

--- cypress/e2e/Stepper.cy.ts
+++ cypress/e2e/Stepper.cy.ts
@@ -6,6 +6,21 @@ describe('First test', () => {
       cy.get('[class="menu-title"]').contains("Stepper").click()
       cy.get('.sidebar-toggle').click()
       cy.get('[type="submit"]').contains("next").click()
-  
+
+     // cy.get('[class="col-md-12 col-lg-12 col-xxxl-12"]')
+     // .children('[class="ng-star-inserted"]').should('have.text', 'Step content #2')
+     cy.get('.horizontal > .step-content > h3.ng-star-inserted').should('have.text', 'Step content #2')
+     // cy.get('[class="ng-star-inserted"]').should('have.text', 'Step content #1')
+
+     cy.get('[type="submit"]').contains("next").click()
+     cy.get('.horizontal > .step-content > h3.ng-star-inserted').should('have.text', 'Step content #3')
+
+
+     cy.get('[type="submit"]').contains("next").click()
+     cy.get('.horizontal > .step-content > h3.ng-star-inserted').should('have.text', 'Step content #4')
+
+     cy.get('[type="submit"]').contains("next").should('be.disabled')
+
+    
      })
   })   
